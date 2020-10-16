76702a44596735b6909e8455d43b62c2034eb707
Anwesh43 <anweshthecool0@gmail.com>
2020-10-17 04:45:13 +0530

    adding Renderer class

--- torchrectview/src/main/java/com/anwesh/uiprojects/torchrectview/TorchRectView.kt
+++ torchrectview/src/main/java/com/anwesh/uiprojects/torchrectview/TorchRectView.kt
@@ -202,4 +202,27 @@ class TorchRectView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : TorchRectView) {
+
+        private val animator : Animator = Animator(view)
+        private val tr : TorchRect = TorchRect(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            tr.draw(canvas, paint)
+            animator.animate {
+                tr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            tr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
